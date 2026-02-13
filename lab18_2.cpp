f29583d2e7bee7d340a31606c92eb8e246b7fe83
nichapat-GG <nichapat.farsai@gmail.com>
2026-02-13 13:49:55 +0700

    finish

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -2,11 +2,39 @@
 using namespace std;
 
 struct Rect{
-	double x,y,w,h;
+    double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1, Rect *R2){
 
+    double L, R, B, T;
 
+    if(R1->x > R2->x){
+        L = R1->x;
+    }else{
+        L = R2->x;
+    }
 
+    if(R1->x + R1->w < R2->x + R2->w){
+        R = R1->x + R1->w;
+    }else{
+        R = R2->x + R2->w;
+    }
+
+    if(R1->y < R2->y){
+        T = R1->y;
+    }else{
+        T = R2->y;
+    }
+
+    if(R1->y - R1->h > R2->y - R2->h){
+        B = R1->y - R1->h;
+    }else{
+        B = R2->y - R2->h;
+    }
+
+	if(R <= L || T <= B){
+		return 0;
+	}
+	return (R - L)*(T - B);
 }
